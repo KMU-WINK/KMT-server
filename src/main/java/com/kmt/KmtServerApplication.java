0a799b2509a74b683b052d37a290a843380a9f08
SuperH0ng <ghdtmdgus100@gmail.com>
2023-08-19 19:49:27 +0900

    NO ISSUE

--- src/main/java/com/kmt/KmtServerApplication.java
+++ src/main/java/com/kmt/KmtServerApplication.java
@@ -2,7 +2,9 @@ package com.kmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class KmtServerApplication {
 
---
+++ src/main/java/com/kmt/controller/RestaurantController.java
@@ -0,0 +1,20 @@
+package com.kmt.controller;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restaurant")
+@RequiredArgsConstructor
+public class RestaurantController {
+
+    @GetMapping("{id}")
+    @ApiOperation("test")
+    public String getSoup(@PathVariable final Long id) {
+        return "test";
+    }
+}
