67efb13d20442085f2503a50e412165b0adf3c37
SuperH0ng <ghdtmdgus100@gmail.com>
2023-08-19 18:09:37 +0900

    feat : init

---
+++ src/main/java/com/kmt/KmtServerApplication.java
@@ -0,0 +1,13 @@
+package com.kmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmtServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmtServerApplication.class, args);
+	}
+
+}
